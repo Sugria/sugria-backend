24f1819a3de4a4f7ba456cea23d033e205f7d7dc
60minuteman <6teeminuteman@gmail.com>
2025-03-04 13:55:41 +0100

    fix: update jest configuration
    - Use TypeScript jest config
    - Move @types/jest to dependencies
    - Update test scripts
    - Remove old jest.config.js

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from 'jest';
+import type { JestConfigWithTsJest } from 'ts-jest';
 
-const config: Config = {
+const config: JestConfigWithTsJest = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
